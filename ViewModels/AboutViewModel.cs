76861a1c559da58987b56abcc37d8234bb5183e1
Caleb Yang <caleb.yanghannchih93@gmail.com>
2021-05-31 17:32:53 +0800

    Create AboutViewModel.cs

---
+++ ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFApp1.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://xamarin.com"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
