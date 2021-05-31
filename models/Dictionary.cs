b8a5efb52d78b38b23e441e820dedf9cafe0d06e
Caleb Yang <caleb.yanghannchih93@gmail.com>
2021-05-31 17:10:43 +0800

    Create Dictionary.cs

---
+++ models/Dictionary.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFApp1.Models
+{   
+    
+    public class Dictionary
+    {
+        public string searchWord { get; set; }
+        public string countryCode { get; set; }
+        public string dictionaryVersion { get; set; }
+        public DateTime yearPublished { get; set; }
+    }
+}
