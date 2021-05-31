a4dd5b767ae83c5c91a80412bb2668c11d289c8e
Caleb Yang <caleb.yanghannchih93@gmail.com>
2021-05-31 18:46:23 +0800

    Create ItemDetailViewModel.cs

---
+++ ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+using XFApp1.Models;
+using XFApp1.ViewModels;
+
+namespace XFApp1.ViewModels
+{
+    [QueryProperty(nameof(ItemId), nameof(ItemId))]
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        private string itemId;
+        private string text;
+        private string description;
+        private DateTime enteredOn;
+        private DateTime updatedOn;
+        private DateTime deletedOn;
+        public string Id { get; set; }
+
+        public string Text
+        {
+            get => text;
+            set => SetProperty(ref text, value);
+        }
+
+        public string Description
+        {
+            get => description;
+            set => SetProperty(ref description, value);
+        }
+
+        public DateTime EnteredOn
+        {
+            get => enteredOn;
+            set => SetProperty(ref enteredOn, value);
+        }
+
+        public string ItemId
+        {
+            get
+            {
+                return itemId;
+            }
+            set
+            {
+                itemId = value;
+                LoadItemId(value);
+            }
+        }
+
+        public async void LoadItemId(string itemId)
+        {
+            try
+            {
+                var item = await DataStore.GetItemAsync(itemId);
+                Id = item.Id;
+                Text = item.Text;
+                Description = item.Description;
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("Failed to Load Item");
+            }
+        }
+    }
+}
