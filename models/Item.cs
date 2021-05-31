417de7c73c4b110ecd7c264472cf1f8e6cdab7c8
Caleb Yang <caleb.yanghannchih93@gmail.com>
2021-05-31 17:11:37 +0800

    Create Item.cs

---
+++ models/Item.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace XFApp1.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public DateTime EnteredOn { get; set; }
+        public DateTime RemovedOn { get; set; }
+    
+    }
+}
